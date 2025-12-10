b7edd75d81aad798833fe8852994e0ac8ddb1d7e
Fris El <uxfris@gmail.com>
2025-12-10 02:31:56 -0500

    feat: Add cursor-pointer to select and slider components, and update tab active/hover styles.

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -18,8 +18,8 @@ function Slider({
       Array.isArray(value)
         ? value
         : Array.isArray(defaultValue)
-        ? defaultValue
-        : [min, max],
+          ? defaultValue
+          : [min, max],
     [value, defaultValue, min, max]
   );
 
@@ -53,7 +53,7 @@ function Slider({
         <SliderPrimitive.Thumb
           data-slot="slider-thumb"
           key={index}
-          className="border-primary ring-ring/50 block size-4 shrink-0 rounded-full bg-primary shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50"
+          className="border-primary ring-ring/50 block size-4 shrink-0 rounded-full bg-primary shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50 cursor-pointer"
         />
       ))}
     </SliderPrimitive.Root>
