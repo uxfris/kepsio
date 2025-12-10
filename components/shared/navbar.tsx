c74ac12770824067655199c0097accae3ea39783
Fris El <uxfris@gmail.com>
2025-12-10 04:58:13 -0500

    feat: Add active navigation link styling

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -1,17 +1,25 @@
+"use client"
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { cn } from "@/lib/utils"
 import Image from 'next/image'
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export function Navbar() {
+    const pathName = usePathname();
+
+    const isActive = (path: string) => pathName === path
+
 
     return <nav className="bg-background px-8 py-2 sticky top-0 z-50 border-b">
         <div className="flex items-center justify-between">
             <Image src="/kepsio.svg" width={94} height={28} alt={'kepsio'} />
             <div className="hidden sm:ml-7 sm:block sm:grow">
                 <div className="flex space-x-8 text-sm">
-                    <Link href={"/generate"}>Generate</Link>
-                    <Link href={"/library"}>Library</Link>
-                    <Link href={"/voice"}>Voice</Link>
+                    <Link href={"/generate"} className={cn(isActive("/generate") && "text-accent")}>Generate</Link>
+                    <Link href={"/library"} className={cn(isActive("/library") && "text-accent")}>Library</Link>
+                    <Link href={"/voice"} className={cn(isActive("/voice") && "text-accent")}>Voice</Link>
                 </div>
             </div>
             <div className="flex items-center justify-center gap-2 px-3 pt-2 pb-[10px] rounded-xl bg-secondary mr-3">
