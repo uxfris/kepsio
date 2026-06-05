088672a3ee6defb751b6742d8b859aececab2c91
uxfris <uxfris@gmail.com>
2026-06-05 10:46:14 -0400

    feat: Add library and voice pages with placeholder content and an empty icon

---
+++ app/(dashboard)/library/page.tsx
@@ -0,0 +1,12 @@
+export default function library() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-5 w-screen h-screen text-center">
+      <img src="/empty-icon.svg" alt="Empty Icon" />
+      <h1 className="text-2xl font-heading">Pardon our dust!</h1>
+      <p className="text-sm">
+        We're busy polishing this page to make it perfect for you. Check back
+        again soon.
+      </p>
+    </div>
+  );
+}
